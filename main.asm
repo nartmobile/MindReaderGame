1d5eacf04ee5bbb7c742d285888f79796f2fa029
nam-t24 <namtruong831@gmail.com>
2021-10-26 18:42:12 -0500

    Initial asm commit

--- main.asm
+++ main.asm
@@ -1,3 +1,16 @@
 .data
-  poopee: .asciiz "Andrew is supreme"
+	cardInitialValues: .word 1, 2, 4, 8, 16, 32
 .text
+
+	#Randomizing array
+	
+		
+	
+		
+	#Looping through array to print cards
+	
+		#print cards function call
+		
+		
+		
+	#Print cards function
