06d0574800d274bec2e3596fde5f9f19b45983c7
nam-t24 <namtruong831@gmail.com>
2021-10-27 15:13:33 -0500

    Add play again loop

--- main.asm
+++ main.asm
@@ -1,8 +1,11 @@
 .data
 	cardInitialValues: .word 1, 2, 4, 8, 16, 32
+	playAgainMessage: .asciiz "\n\nDo you want to play again? (1 for yes, 0 for no)\n"
+	newLine: .asciiz "\n"
 	
 .text
 
+beginGame:
 	#Randomizing array
 	
 	li $t0, 5
@@ -51,6 +54,7 @@ PrintArray:
 	li $v0, 11
 	syscall
 	
+	
 	addi $t0, $t0, 4
 	j PrintArray
 	
@@ -61,6 +65,19 @@ exitLoop:
 	
 		#print cards function call
 		
-		
-		
+	#asking to play again
+	li $v0, 4
+	la $a0, playAgainMessage
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	beqz $v0, exitGame
+	j beginGame
+	
+exitGame:
+	li $v0, 10
+	syscall
+	
 	#Print cards function
